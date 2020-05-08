15c12cd6b4a082adda99d41ec5631e2ca1dd9e74
Thibault MARECHAL <tml@terawatt.fr>
2020-05-08 10:57:48 +0200

    Augment Frequency of transmission, don't restrict position to a circle around init position.

--- gps_simulator.py
+++ gps_simulator.py
@@ -6,16 +6,20 @@ import config
 client = mqtt.Client()
 client.connect(config.mqtt_server_ip, config.mqtt_server_port, config.mqtt_server_keepalive)
 
+latitude = config.position[0]
+longitude = config.position[1]
+altitude = config.position[2]
+
 while True:
     randomval = random.uniform(config.range[0], config.range[1])
     randomval2 = random.uniform(config.range[0], config.range[1])
 
-    latitude = config.position[0] + randomval
-    longitude = config.position[1] + randomval2
-    altitude = config.position[2]
+    latitude += randomval
+    longitude += randomval2
+
     timestamp = int(time.time())
     simu = '{{"name":{0},"lat":{1}, "lon":{2}, "alt":{3}, "ts":{4}}}'.format(config.name, latitude, longitude, altitude, timestamp)
     print(simu)
     client.publish("tracker", simu)
-    time.sleep(5)
+    time.sleep(2)
 client.disconnect()
